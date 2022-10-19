b35692c1a7c22ec9bdc4ea82793d9ca5efa9ff1d
Laurie Roy <laurieroy.dev@gmail.com>
2022-10-19 12:44:29 -0700

    Write simple and custom validations for user, activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,25 @@
 class Activity < ApplicationRecord
   belongs_to :user
 
-  enum category: [:run, :long_run, :race, :workout, :other]
-  enum difficulty: [:easy, :moderate, :hard]
-  enum unit: [:miles, :kilometer, :meters]
+  enum category: %i[run paddle hike workout race ride swim other]
+  enum difficulty: %i[easy moderate hard]
+  enum unit: %i[miles kilometers meters]
+
+  validates :date, presence: true
+  validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
+  validates :distance, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+
+  validate :require_distance_or_duration
+  validate :require_unit_if_set_distance
+
+  private 
+
+  def require_distance_or_duration
+    errors.add(:base, "Please add either a distance or a duration") if self.distance.nil? && self.duration.nil?
+  end
+
+  def require_unit_if_set_distance
+    errors.add(:base, "Please select a unit for distance") if self.distance.present? && self.unit.nil? 
+  end
+  
 end
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class ActivityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:confirmed_user)
+    @activity = @user.activities.build(date: Time.zone.now, duration: 1)
+  end
+
+  test "should be valid" do
+    assert @activity.valid?
+  end
+
+  test "should have a date" do
+    @activity.date = nil
+    assert_not @activity.valid?
+  end
+
+  test "duration should be positive" do
+    @activity.duration = -1
+    assert_not @activity.valid?
+  
+    @activity.duration = 0
+    assert_not @activity.valid?
+  end
+
+  test "distance should be positive" do
+    @activity.distance  = -1
+    assert_not @activity.valid?
+ 
+    # @activity.distance  = 0
+    # assert_not @activity.valid?
+  end
+
+  test "has either distance or duration" do
+    @activity.distance  = nil
+    @activity.duration  = nil
+
+    assert_not @activity.valid?
+  end
+
+  test "distancce has unit if distance is set" do
+    @activity.distance  = 1
+    @activity.unit = nil
+
+    assert_not @activity.valid?
+  end
+
+  
 end
