09ed435fda32a316d9405adbf67ea2d98a39b883
Laurie Roy <laurieroy.dev@gmail.com>
2022-10-19 14:39:20 -0700

    Add calculated pace column to activity and test

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,9 +1,11 @@
 class Activity < ApplicationRecord
   belongs_to :user
-
+  
   enum category: %i[run paddle hike workout race ride swim other]
   enum difficulty: %i[easy moderate hard]
   enum unit: %i[miles kilometers meters]
+
+  before_save :calculate_pace
 
   validates :date, presence: true
   validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
@@ -13,6 +15,27 @@ class Activity < ApplicationRecord
   validate :require_unit_if_set_distance
 
   private 
+
+  def calculate_pace
+		# distances are converted to miles before calculating pace
+		kmConversionFactor = 0.621371
+		mConversionFactor = 0.000621371
+
+		if self.unit.present? && self.distance.present? && self.duration.present?
+			case self.unit
+			when "miles"
+				self.calculated_pace = self.duration / self.distance
+
+			when "kilometers"
+				distance_in_miles = self.distance * kmConversionFactor
+				self.calculated_pace = self.duration / (self.distance * kmConversionFactor)
+
+			when "meters"
+				distance_in_miles = self.distance * mConversionFactor
+				self.calculated_pace = self.duration / (self.distance * mConversionFactor)
+			end
+		end
+	end
 
   def require_distance_or_duration
     errors.add(:base, "Please add either a distance or a duration") if self.distance.nil? && self.duration.nil?
--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -4,6 +4,8 @@ class ActivityTest < ActiveSupport::TestCase
   def setup
     @user = users(:confirmed_user)
     @activity = @user.activities.build(date: Time.zone.now, duration: 1)
+    @kmConversionFactor = 0.6213711985
+    @mConversionFactor = 0.0006213711985
   end
 
   test "should be valid" do
@@ -38,7 +40,7 @@ class ActivityTest < ActiveSupport::TestCase
     assert_not @activity.valid?
   end
 
-  test "distancce has unit if distance is set" do
+  test "distance has unit if distance is set" do
     @activity.distance  = 1
     @activity.unit = nil
 
@@ -46,4 +48,37 @@ class ActivityTest < ActiveSupport::TestCase
   end
 
   
+  test "calculates pace given a distance (miles) and duration (s)" do
+    @activity.distance = 10
+    @activity.unit = :miles
+    @activity.duration = 3600
+    @activity.save
+
+    assert_equal 360, @activity.reload.calculated_pace
+  end 
+
+
+  test "calculates pace given a distance (kilometers) and duration (s)" do
+    @activity.distance = 10
+    @activity.unit = :kilometers
+    @activity.duration = 1800
+    @activity.save
+
+    distance_in_miles = @activity.distance * @kmConversionFactor
+    pace = @activity.duration / distance_in_miles
+
+    assert_in_delta pace, @activity.reload.calculated_pace, 0.001
+  end
+
+  test "calculates pace (sec/mi) given a distance (meters) and duration (seconds)" do
+    @activity.distance = 800
+    @activity.unit = :meters
+    @activity.duration = 180
+    @activity.save
+
+    distance_in_miles = @activity.distance * @mConversionFactor
+    pace = @activity.duration / distance_in_miles
+
+    assert_in_delta pace, @activity.reload.calculated_pace, 0.001
+  end
 end
