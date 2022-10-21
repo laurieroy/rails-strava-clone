9816466d20a68a95aed0e66bd49f37a7a83791bd
Laurie Roy <laurieroy.dev@gmail.com>
2022-10-21 12:31:22 -0700

    Build out activites CRUD UI

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,10 @@
 class ActivitiesController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_activity, except: %i[index new create]
-	before_action :calculate_duration
+	before_action :calculate_duration, only: [:create]
 
 	def index
+		@activities = current_user.activities.all
 	end
 	
 	def show 
@@ -33,22 +34,36 @@ class ActivitiesController < ApplicationController
 		end
 	end
 
-
+	def destroy
+		@activity.destroy
+		redirect_to activities_path, notice: "Activity deleted"
+	end
+	
 	private 
 
 	def activity_params
 		params.require(:activity).permit(:duration, :distance, :category, :difficulty, :unit, :date, :description)
 	end
 
+	# def calculate_duration
+	# 	calculated_duration = 0
+		
+	# 	calculated_duration += params[:activity][:hours].to_i * 3600 if params[:activity][:hours].present?
+	# 	calculated_duration += params[:activity][:minutes].to_i * 60 if params[:activity][:minutes].present?
+	# 	calculated_duration += params[:activity][:seconds].to_i if params[:activity][:seconds].present?
+
+	# 	params[:activity][:duration] = calculated_duration unless calculated_duration.zero?
+	# end
+
 	def calculate_duration
 		calculated_duration = 0
-		
-		calculated_duration += params[:activity][:hours].to_i * 3600 if params[:activity][:hours].present?
+
+		calculated_duration += params[:activity][:hours].to_i * (60 * 60) if params[:activity][:hours].present?
 		calculated_duration += params[:activity][:minutes].to_i * 60 if params[:activity][:minutes].present?
 		calculated_duration += params[:activity][:seconds].to_i if params[:activity][:seconds].present?
 
-		params[:activity][:duration] = calculated_duration unless calculated_duration.zero?
-	end
+		params[:activity][:duration] = calculated_duration unless calculated_duration == 0
+end
 	
 	def set_activity 
 		@activity = Activity.find(params[:id])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def format_time(seconds)
 		Time.at(seconds).utc.strftime("%H:%M:%S")
 	end
+
+	def format_date_time(seconds)
+		Time.at(seconds).utc.strftime("%D %r")
+	end
 end
