e3f3eabfacca7a652329e12a3fc54899e2d5a36d
Laurie Roy <laurieroy.dev@gmail.com>
2022-10-23 17:53:00 -0700

    Refactor duration
    Add hours, minutes, seconds columns; move calculate_duration to model where it should be; Update fixture

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,6 @@
 class ActivitiesController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_activity, except: %i[index new create]
-	before_action :calculate_duration, only: [:create]
 
 	def index
 		@activities = current_user.activities.all
@@ -46,28 +45,8 @@ class ActivitiesController < ApplicationController
 	private 
 
 	def activity_params
-		params.require(:activity).permit(:duration, :distance, :category, :difficulty, :unit, :date, :description)
+		params.require(:activity).permit(:distance, :category, :difficulty, :unit, :date, :description, :hours, :minutes, :seconds)
 	end
-
-	# def calculate_duration
-	# 	calculated_duration = 0
-		
-	# 	calculated_duration += params[:activity][:hours].to_i * 3600 if params[:activity][:hours].present?
-	# 	calculated_duration += params[:activity][:minutes].to_i * 60 if params[:activity][:minutes].present?
-	# 	calculated_duration += params[:activity][:seconds].to_i if params[:activity][:seconds].present?
-
-	# 	params[:activity][:duration] = calculated_duration unless calculated_duration.zero?
-	# end
-
-	def calculate_duration
-		calculated_duration = 0
-
-		calculated_duration += params[:activity][:hours].to_i * (60 * 60) if params[:activity][:hours].present?
-		calculated_duration += params[:activity][:minutes].to_i * 60 if params[:activity][:minutes].present?
-		calculated_duration += params[:activity][:seconds].to_i if params[:activity][:seconds].present?
-
-		params[:activity][:duration] = calculated_duration unless calculated_duration == 0
-end
 	
 	def set_activity 
 		@activity = Activity.find(params[:id])
