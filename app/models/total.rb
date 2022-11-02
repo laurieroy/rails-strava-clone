82916b7ca9a2eca7c47ac3b03233e246f9861ef7
Laurie Roy <laurieroy.dev@gmail.com>
2022-11-01 21:33:42 -0700

    Add and test associations and validation to total model

--- app/models/total.rb
+++ app/models/total.rb
@@ -2,4 +2,7 @@ class Total < ApplicationRecord
   belongs_to :user
 
   enum range: %i[week]
+
+  validates :range, :start_date, presence: true
+
 end
--- test/models/total_test.rb
+++ test/models/total_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class TotalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:confirmed_user)
+    @total = @user.totals.build(start_date: Time.zone.now, range: 'week')
+  end
+  
+  test "should be valid" do
+    assert @total.valid?
+  end
+
+  test "should have a start_date" do
+    @total.start_date = nil
+    assert_not @total.valid?
+  end
+
+  test "should have a range value" do
+    @total.range = nil
+    assert_not @total.valid?
+  end
 end
