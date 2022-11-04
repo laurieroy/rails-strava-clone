695ea0d0db0ad2a54bc92fe16f17d6cca29bcd34
Laurie Roy <laurieroy.dev@gmail.com>
2022-11-04 11:05:13 -0700

    Shoe does not lose mileage when activity destroyed

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -159,4 +159,13 @@ class ActivityTest < ActiveSupport::TestCase
 
     assert_equal 10, @shoe.reload.distance_in_miles
   end
+# Note: I'm differing from the tutorial here in that if a shoe has activity, I'm not removing the miles when the activity is deleted. If needed can add in a special method
+  test "does not update associated shoes distance_in_miles value when activity is destoyed" do
+    @user = users(:confirmed_user_with_shoes)
+    @shoe = @user.shoes.first
+    @activity = @user.activities.create!(distance: 10, unit: "miles", shoe:@shoe, date: Time.zone.now)
+    @activity.destroy
+
+    assert_equal 10, @shoe.reload.distance_in_miles
+  end
 end
