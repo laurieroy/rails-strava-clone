e204f58a127c674d3d15212cb8ecd590882cf943
Laurie Roy <laurieroy.dev@gmail.com>
2022-11-04 12:59:24 -0700

    Add notified column to shoes table; create failing test

---
+++ test/integration/notify_users_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class NotifyUsersTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:confirmed_user_with_shoes)
+    @shoe = shoes(:confirmed_user_with_shoes_shoe_10)
+  end
+
+  test "notifies user when replacement_miles is reached" do
+    difference = @shoe.replacement_miles - @shoe.distance_in_miles
+
+    assert_emails 1 do
+      post activities_path, params: { activity: {distance: difference, unit: "miles", user:@user, shoe:@shoe, date: Time.zone.now}}
+    end
+  end
+end
